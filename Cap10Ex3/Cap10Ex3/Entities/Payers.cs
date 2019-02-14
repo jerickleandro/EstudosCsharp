85b41940b39d54d2036549961668cff030e31a37
unknown <j.erickleandro@gmail.com>
2019-02-14 18:31:06 -0300

    Novo exercicio de herança

---
+++ Cap10Ex3/Cap10Ex3/Entities/Payers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap10Ex3.Entities {
+    abstract class Payers {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public Payers() { }
+
+        public Payers(string name, double anualIncome) {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+
+    }
+}
